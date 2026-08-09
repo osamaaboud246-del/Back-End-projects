deac82e91b2d40e26833c210e3174fa40c64d97e
osama <osamaaboud246@gmail.com>
2026-08-09 16:03:11 +0300

    complete lab 18

---
+++ Lab_18/StudentPortalWeb/Views/Shared/_StudentCard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentPortalWeb.Views.Shared
+{
+    public class _StudentCardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
